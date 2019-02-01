984b132003d1945a42226a3cdc3c2d9a691a34ad
Floofies <fantasticdragons@gmail.com>
2019-02-01 04:14:28 -0500

    Switch from microbot to telegraf

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
-const babel = require("@babel/parser");
-const microBot = require("micro-bot");
-const Composer = microBot.Composer;
-const log = microBot.log;
-const session = microBot.session;
-const bot = new microBot.Composer();
-bot.use(microBot.log());
-bot.use(microBot.session());
+const babel = require("babel-core");
+const Telegraf = require("telegraf");
+const bot = new Telegraf(process.env.BOT_TOKEN);
+function monospace(str) {
+	return "<code>" + str + "</code>";
+}
+function locPropRemover(path) {
+	delete path.node.start;
+	delete path.node.end;
+	delete path.node.loc;
+}
+function addVisitor(types, visitor) {
+	for (const type of types) visitor[type] = locPropRemover;
+}
+function locRemover(babel) {
+	const visitor = {};
+	addVisitor(babel.types.DECLARATION_TYPES, visitor);
+	addVisitor(babel.types.STATEMENT_TYPES, visitor);
+	addVisitor(babel.types.EXPRESSION_TYPES, visitor);
+	addVisitor(babel.types.LITERAL_TYPES, visitor);
+	return {visitor: visitor};
+}
 bot.start(ctx => ctx.reply("Send me JavaScript source code, and I'll give you the Babel Abstract Syntax Tree!"));
 bot.on("message", function (ctx) {
+	console.log(ctx.message);
 	try {
-		ctx.reply(babel.parse(ctx.message));
+		const ast = babel.transform(ctx.message.text, {
+			plugins: [locRemover]
+		}).ast.program;
+		delete ast.loc;
+		delete ast.start;
+		delete ast.end;
+		ctx.replyWithHTML(monospace(JSON.stringify(ast, null, "\t")));
 	} catch (error) {
-		ctx.reply(error.message);
+		ctx.replyWithHTML(monospace(error.message));
+		console.error(error);
 	}
 });
-module.exports = bot;
+bot.launch();
